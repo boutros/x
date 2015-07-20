49719d20c5b8648195f708e9c4fc41e9289eb12b
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2015-07-20 23:10:05 +0200

    More work on parser

--- malle/rdf/nt.go
+++ malle/rdf/nt.go
@@ -12,35 +12,53 @@ type NTDecoder struct {
 }
 
 // NewNTDecoder returns a new NTDecoder on the given stream.
+//
+// Notes and deviations from W3 specification:
+// * IRIs are not validated, except to make sure they are not empty.
+// * Literal values are not validated against their datatype.
+// * Any tokens after triple termination (.) until end of line are ignored.
+// * Triples with blank nodes are ignored.
 func NewNTDecoder(r io.Reader) *NTDecoder {
 	return &NTDecoder{lex: newLexer(r)}
+}
+
+func (d *NTDecoder) ignoreLine() {
+	for tok := d.lex.next(); tok.Typ != tokenEOL && tok.Typ != tokenEOF; tok = d.lex.next() {
+	}
 }
 
 func (d *NTDecoder) parseSubject() (token, error) {
 	var tok token
 	for tok = d.lex.next(); tok.Typ == tokenEOL; tok = d.lex.next() {
 	}
-	if tok.Typ == tokenError {
+	switch tok.Typ {
+	case tokenError:
 		return token{}, errors.New(string(tok.value))
-	}
-	if tok.Typ == tokenEOF {
+	case tokenEOF:
 		return token{}, io.EOF
-	}
-	if tok.Typ != tokenIRI {
+	case tokenBNode:
+		return tok, nil
+	case tokenIRI:
+		break
+	default:
 		return token{}, fmt.Errorf("expected IRI as subject, got %s", tok.Typ.String())
 	}
+
 	return tok, nil
 }
 
 func (d *NTDecoder) parsePredicate() (token, error) {
 	tok := d.lex.next()
-	if tok.Typ == tokenError {
+	switch tok.Typ {
+	case tokenError:
 		return token{}, errors.New(string(tok.value))
-	}
-	if tok.Typ == tokenEOF {
+	case tokenEOF:
 		return token{}, io.EOF
-	}
-	if tok.Typ != tokenIRI {
+	case tokenBNode:
+		return tok, nil
+	case tokenIRI:
+		break
+	default:
 		return token{}, fmt.Errorf("expected IRI as predicate, got %s", tok.Typ.String())
 	}
 	return tok, nil
@@ -48,39 +66,53 @@ func (d *NTDecoder) parsePredicate() (token, error) {
 
 func (d *NTDecoder) parseObject() (token, error) {
 	tok := d.lex.next()
-	if tok.Typ == tokenError {
+	switch tok.Typ {
+	case tokenError:
 		return token{}, errors.New(string(tok.value))
-	}
-	if tok.Typ == tokenEOF {
+	case tokenEOF:
 		return token{}, io.EOF
-	}
-	if tok.Typ != tokenIRI && tok.Typ != tokenLiteral {
+	case tokenIRI, tokenLiteral, tokenBNode:
+		break
+	default:
 		return token{}, fmt.Errorf("expected IRI or Literal as object, got %s", tok.Typ.String())
 	}
+
 	return tok, nil
 }
 
-func (d *NTDecoder) parseDot() error {
+func (d *NTDecoder) parseEnd() error {
+	// Each statement must end in a dot (.)
 	tok := d.lex.next()
-	if tok.Typ == tokenError {
+	switch tok.Typ {
+	case tokenError:
 		return errors.New(string(tok.value))
-	}
-	if tok.Typ == tokenEOF {
+	case tokenEOF:
 		return io.EOF
-	}
-	if tok.Typ != tokenDot {
+	case tokenDot:
+		break
+	default:
 		return fmt.Errorf("expected dot, got %s", tok.Typ.String())
 	}
+
+	// Any tokens after dot until end of line are ignored
+	d.ignoreLine()
+
 	return nil
 }
 
 // Decode returns the next valid triple in the the stream, or an error.
 func (d *NTDecoder) Decode() (Triple, error) {
 	var tr Triple
+newLine:
 	// subject
 	tok, err := d.parseSubject()
 	if err != nil {
+		d.ignoreLine()
 		return Triple{}, err
+	}
+	if tok.Typ == tokenBNode {
+		d.ignoreLine()
+		goto newLine
 	}
 
 	b := make([]byte, len(tok.value)+1)
@@ -90,8 +122,14 @@ func (d *NTDecoder) Decode() (Triple, error) {
 	// predicate
 	tok, err = d.parsePredicate()
 	if err != nil {
+		d.ignoreLine()
 		return Triple{}, err
 	}
+	if tok.Typ == tokenBNode {
+		d.ignoreLine()
+		goto newLine
+	}
+
 	b = make([]byte, len(tok.value)+1)
 	copy(b[1:], tok.value)
 	tr.pred = IRI{val: b}
@@ -99,7 +137,12 @@ func (d *NTDecoder) Decode() (Triple, error) {
 	// object
 	tok, err = d.parseObject()
 	if err != nil {
+		d.ignoreLine()
 		return Triple{}, err
+	}
+	if tok.Typ == tokenBNode {
+		d.ignoreLine()
+		goto newLine
 	}
 	if tok.Typ == tokenIRI {
 		b = make([]byte, len(tok.value)+1)
@@ -109,9 +152,10 @@ func (d *NTDecoder) Decode() (Triple, error) {
 		panic("TODO parse Literal object")
 	}
 
-	// dot
-	err = d.parseDot()
+	// dot+newline/eof
+	err = d.parseEnd()
 	if err != nil {
+		d.ignoreLine()
 		return Triple{}, err
 	}
 
--- malle/rdf/nt_test.go
+++ malle/rdf/nt_test.go
@@ -24,10 +24,54 @@ func TestDecodeNT(t *testing.T) {
 		errWant []error
 	}{
 		{
+			"",
+			[]Triple{},
+			[]error{},
+		},
+		{
+			"\n#comment <a> <b> <c> .\n",
+			[]Triple{},
+			[]error{},
+		},
+		{
 			"<s><p><o>.\n<s><p><o2>.",
 			[]Triple{
 				Triple{subj: mustNewIRI("s"), pred: mustNewIRI("p"), obj: mustNewIRI("o")},
 				Triple{subj: mustNewIRI("s"), pred: mustNewIRI("p"), obj: mustNewIRI("o2")}},
+			[]error{},
+		},
+		{
+			"\n\n<s>\t<p> <o>.#comment\n#commment\n<s><p><o2>.",
+			[]Triple{
+				Triple{subj: mustNewIRI("s"), pred: mustNewIRI("p"), obj: mustNewIRI("o")},
+				Triple{subj: mustNewIRI("s"), pred: mustNewIRI("p"), obj: mustNewIRI("o2")}},
+			[]error{},
+		},
+		{
+			"<s><p><o>.<z>\n",
+			[]Triple{Triple{subj: mustNewIRI("s"), pred: mustNewIRI("p"), obj: mustNewIRI("o")}},
+			[]error{},
+		},
+		{
+			"<s><p><o>.<z>\n<s><p><o2>.<y>",
+			[]Triple{
+				Triple{subj: mustNewIRI("s"), pred: mustNewIRI("p"), obj: mustNewIRI("o")},
+				Triple{subj: mustNewIRI("s"), pred: mustNewIRI("p"), obj: mustNewIRI("o2")}},
+			[]error{},
+		},
+		{
+			"_:b1 <p> <o> .",
+			[]Triple{},
+			[]error{},
+		},
+		{
+			"<s> <p> _:b2 .",
+			[]Triple{},
+			[]error{},
+		},
+		{
+			"<s> <p> _:b2 .\n<S><P><O>.\n\t \n",
+			[]Triple{Triple{subj: mustNewIRI("S"), pred: mustNewIRI("P"), obj: mustNewIRI("O")}},
 			[]error{},
 		},
 	}
@@ -35,15 +79,23 @@ func TestDecodeNT(t *testing.T) {
 	for _, test := range tests {
 		dec := NewNTDecoder(bytes.NewBufferString(test.input))
 		trs, errs := collectTrErr(dec)
-		for i, tr := range test.trWant {
-			if !trs[i].Eq(tr) {
-				t.Fatalf("decoding:\n%q\ngot:\n%v\nwant:\n%v", test.input, trs, test.trWant)
+		if len(trs) != len(test.trWant) {
+			t.Errorf("decoding:\n%q\ngot:\n%v\nwant:\n%v", test.input, trs, test.trWant)
+		} else {
+			for i, tr := range test.trWant {
+				if !trs[i].Eq(tr) {
+					t.Errorf("decoding:\n%q\ngot:\n%v\nwant:\n%v", test.input, trs, test.trWant)
+				}
 			}
 		}
 
-		for i, err := range test.errWant {
-			if errs[i] != err {
-				t.Fatalf("decoding:\n%q\ngot:\n%v\nwant:\n%v", test.input, errs, test.errWant)
+		if len(errs) != len(test.errWant) {
+			t.Errorf("decoding:\n%q\ngot:\n%v\nwant:\n%v", test.input, errs, test.errWant)
+		} else {
+			for i, err := range test.errWant {
+				if errs[i] != err {
+					t.Errorf("decoding:\n%q\ngot:\n%v\nwant:\n%v", test.input, errs, test.errWant)
+				}
 			}
 		}
 	}
