c788cff0593a638a2ec9a876a9524d34b6b54b28
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2017-03-09 20:32:08 +0100

    kobleren: start work on graph triple pattern matching (WIP, broken)

--- kobleren/elm/src/RDF/Graph.elm
+++ kobleren/elm/src/RDF/Graph.elm
@@ -1,10 +1,13 @@
-module RDF.Graph exposing (Graph, empty, toNTriples, fromNTriples)
+module RDF.Graph exposing (Graph, empty, toNTriples, fromNTriples, query)
 
-import RDF.RDF exposing (TriplePattern, Term, serialize)
+import RDF.RDF exposing (..)
 import RDF.Parser exposing (parseNTriples)
 import Dict
 import Set
 import List
+
+
+-- TYPES
 
 
 type alias Graph =
@@ -15,9 +18,125 @@ type alias Graph =
     }
 
 
+
+-- EXPORTED FUNCTIONS
+
+
+{-| Create an empty graph.
+-}
 empty : Graph
 empty =
     Graph 0 Dict.empty Dict.empty Dict.empty
+
+
+{-| Parse NTriples into a graph.
+-}
+fromNTriples : String -> Result String Graph
+fromNTriples nt =
+    case parseNTriples nt of
+        Err err ->
+            Result.Err err
+
+        Ok ntriples ->
+            Result.Ok (fromTriples ntriples)
+
+
+{-| Serialize graph as NTriples.
+-}
+toNTriples : Graph -> String
+toNTriples graph =
+    Dict.toList graph.spo
+        |> List.map
+            (\( ( s, p ), o ) ->
+                let
+                    sp =
+                        mustSerialize s graph.id2term
+                            ++ " "
+                            ++ mustSerialize p graph.id2term
+                            ++ " "
+
+                    olist =
+                        Set.toList o
+
+                    -- TODO refactor this mess
+                    all =
+                        \l ->
+                            case l of
+                                [] ->
+                                    Debug.crash "BUG: empty object list in SPO index"
+
+                                [ o1 ] ->
+                                    sp ++ mustSerialize o1 graph.id2term ++ " .\n"
+
+                                o1 :: rest ->
+                                    sp ++ mustSerialize o1 graph.id2term ++ " .\n" ++ all rest
+                in
+                    all
+                        olist
+            )
+        |> List.foldr (++) ""
+
+
+{-| Query the graph for matching triple-patterns.
+-}
+query : List TriplePattern -> Graph -> List TriplePattern
+query patterns graph =
+    List.concatMap (\pattern -> match pattern graph) patterns
+
+
+
+-- QUERY HELPER FUNCTIONS
+
+
+match : TriplePattern -> Graph -> List TriplePattern
+match pattern graph =
+    List.map (\ids -> tripleFromIds ids graph) (matchAll graph)
+
+
+matchAll : Graph -> List ( Int, Int, Int )
+matchAll graph =
+    Dict.toList graph.spo
+        |> List.concatMap
+            (\( ( s, p ), objs ) ->
+                List.map (\o -> ( s, p, o )) (Set.toList objs)
+            )
+
+
+tripleFromIds : ( Int, Int, Int ) -> Graph -> TriplePattern
+tripleFromIds ( s, p, o ) graph =
+    TriplePattern (termFromId s graph) (termFromId p graph) (termFromId o graph)
+
+
+termFromId : Int -> Graph -> Term
+termFromId id graph =
+    case Dict.get id graph.id2term of
+        Nothing ->
+            Debug.crash "BUG: term ID in index but not in dictionary"
+
+        Just s ->
+            termFromString s
+
+
+termFromString : String -> Term
+termFromString s =
+    case (String.slice 0 1 s) of
+        "<" ->
+            TermURI (String.slice 1 -1 s)
+
+        "_" ->
+            TermBlankNode (String.dropLeft 2 s)
+
+        _ ->
+            TermLiteral (literalFromString s)
+
+
+literalFromString : String -> Literal
+literalFromString s =
+    Literal s Nothing xsdString
+
+
+
+-- MUTATING HELPER FUNCTIONS
 
 
 insert1 : TriplePattern -> Graph -> Graph
@@ -71,14 +190,8 @@ indexTriple ( s, p, o ) graph =
         { graph | spo = Dict.insert ( s, p ) newO graph.spo }
 
 
-fromNTriples : String -> Result String Graph
-fromNTriples nt =
-    case parseNTriples nt of
-        Err err ->
-            Result.Err err
 
-        Ok ntriples ->
-            Result.Ok (fromTriples ntriples)
+-- DECODING HELPER FUNCTIONS
 
 
 fromTriples : List TriplePattern -> Graph
@@ -86,37 +199,8 @@ fromTriples triples =
     List.foldr insert1 empty triples
 
 
-toNTriples : Graph -> String
-toNTriples graph =
-    Dict.toList graph.spo
-        |> List.map
-            (\( ( s, p ), o ) ->
-                let
-                    sp =
-                        mustSerialize s graph.id2term
-                            ++ " "
-                            ++ mustSerialize p graph.id2term
-                            ++ " "
 
-                    olist =
-                        Set.toList o
-
-                    all =
-                        \l ->
-                            case l of
-                                [] ->
-                                    Debug.crash "BUG: empty object list in SPO index"
-
-                                [ o1 ] ->
-                                    sp ++ mustSerialize o1 graph.id2term ++ " .\n"
-
-                                o1 :: rest ->
-                                    sp ++ mustSerialize o1 graph.id2term ++ " .\n" ++ all rest
-                in
-                    all
-                        olist
-            )
-        |> List.foldr (++) ""
+-- ENCODING HELPER FUNCTIONS
 
 
 mustSerialize : Int -> Dict.Dict Int String -> String
@@ -130,7 +214,7 @@ mustSerialize id terms =
 
 
 
---maybeToList : Maybe a -> List b
+-- OTHER HELPER FUNCTIONS
 
 
 maybeToSet : Maybe (Set.Set Int) -> Set.Set Int
@@ -144,7 +228,6 @@ maybeToSet m =
 
 
 
---
 -- insert1 -> TriplePattern -> Graph
 -- delete1 -> TriplePattern -> Graph
 -- insert -> List TriplePattern -> Graph
--- kobleren/elm/tests/Graph.elm
+++ kobleren/elm/tests/Graph.elm
@@ -4,10 +4,21 @@ import Test exposing (..)
 import Expect
 import RDF.RDF exposing (..)
 import RDF.Graph as Graph
+import RDF.Parser exposing (parseNTriples)
 
 
-mustParse : String -> Graph.Graph
-mustParse ntriples =
+mustParseTriples : String -> List TriplePattern
+mustParseTriples nt =
+    case parseNTriples nt of
+        Err _ ->
+            []
+
+        Ok triples ->
+            triples
+
+
+mustParseGraph : String -> Graph.Graph
+mustParseGraph ntriples =
     case Graph.fromNTriples ntriples of
         Err err ->
             let
@@ -46,8 +57,35 @@ graph =
                     in
                         Expect.equalLists
                             (sortLines nt)
-                            (sortLines (Graph.toNTriples (mustParse nt)))
+                            (sortLines (Graph.toNTriples (mustParseGraph nt)))
             ]
         , describe "create and manipulate"
             []
+        , describe "querying"
+            [ test "one triple pattern" <|
+                \_ ->
+                    let
+                        nt =
+                            """
+                                <s> <p> <o> .
+                                <s> <p> <o2> .
+                                <s> <p2> <o3> .
+                            """
+
+                        graph =
+                            (mustParseGraph nt)
+                    in
+                        Expect.equalLists
+                            (Graph.query
+                                -- TODO: (mustParseTriples "<s> <p> ?o .")
+                                [ (TriplePattern
+                                    (TermURI "s")
+                                    (TermURI "p")
+                                    (TermVar "o")
+                                  )
+                                ]
+                                graph
+                            )
+                            (mustParseTriples "<s> <p> <o> .\n <s> <p> <o2> .")
+            ]
         ]
